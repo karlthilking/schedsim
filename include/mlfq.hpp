40074f61822924e57ba6fa6afc613b49b1604d0a
Karl Thilking <thilking.k@northeastern.edu>
2026-02-22 18:23:08 -0500

    added helper functions and comments to multi-level feedback queue implementation

---
+++ include/mlfq.hpp
@@ -0,0 +1,147 @@
+#ifndef SCHEDSIM_MLFQ_H
+#define SCHEDSIM_MLFQ_H
+
+namespace scheduler {
+template<size_t N>  // N = number of queues
+class mlfq {
+private:
+    std::array<std::queue<std::unique_ptr<task>>, N> tasks;
+    std::vector<std::thread> threads;
+    std::condition_variable cv;
+    std::array<std::mutex, N> locks;
+    u32 ntasks;
+    milliseconds timeslice;
+    bool stop;
+    
+    /*  
+     *  Get the user and system CPU usage difference beetween
+     *  the previous and current rusage struct in milliseconds;
+     *  when diff >= timeslice the task should be demoted to a 
+     *  lower queue level
+     */
+    u32
+    get_ms_diff(const struct rusage &prev, const struct rusage &cur)
+    {
+        u32 prev_ms = (prev.ru_utime.tv_sec * 1000) + 
+                      (prev.ru_utime.tv_usec / 1000) +
+                      (prev.ru_stime.tv_sec * 1000) + 
+                      (prev.ru_stime.tv_usec / 1000);
+        u32 cur_ms = (cur.ru_utime.tv_sec * 1000) +
+                     (cur.ru_utime.tv_usec / 1000) +
+                     (cur.ru_stime.tv_sec * 1000) +
+                     (cur.ru_stime.tv_usec / 1000);
+        return cur_ms - prev_ms;
+    }
+    
+    /*  
+     *  Run the task for a timeslice and, unless the tasks exits after its
+     *  timeslice, determine at which queue level to reinsert the task
+     */
+    void
+    schedule(task *t) noexcept
+    {
+        assert(t->get_state() != task_state::RUNNING);
+
+        struct rusage prevru = t->ru;
+        if (t->get_state() == task_state::RUNNABLE)
+            t->run();
+        else if (t->get_state() == task_state::STOPPED)
+            kill(t->get_pid(), SIGCONT);
+        t->set_state(task_state::RUNNING);
+
+        std::this_thread::sleep_for(timeslice);
+        kill(t->get_pid(), SIGSTOP);
+
+        int rc, wstat;
+        if ((rc = wait4(t->get_pid(), &wstat, 0, &t->ru)) < 0)
+            err(EXIT_FAILURE, "wait4");
+        else if (rc == 0)
+            err(EXIT_FAILURE, "child state did not change");
+        
+        if (WIFEXITED(wstat)) {
+            t->set_state(task_state::FINISHED);
+            std::cout << t->get_pid() << " exited with exit code "
+                      << WEXITSTATUS(wstat) << '\n';
+        } else if (WIFSTOPPED(wstat)) {
+            t->set_state(task_state::STOPPED);
+            if (get_ms_diff(prevru, t->ru) >= timeslice) {
+                // demote queue level and insert there
+            } else {
+                // keep on the same queue level
+            }
+        }
+        return;
+    }
+
+public:
+    mlfq(u32 ncpus = 1, milliseconds timeslice = 24ms) noexcept
+        : timeslice(timeslice), stop(false), ntasks(0)
+    {
+        threads.reserve(ncpus);
+        for (u32 cpuid = 0; cpuid < ncpus; ++cpuid) {
+            threads.emplace_back([this]{
+                while (true) {
+                    task *t;
+                    // qid = which queue to search
+                    for (u32 level = 0; level < N; ++level) {
+                        std::lock_guard<std::mutex> lock(mtx);
+                        cv.wait(lock, [this]{
+                            return stop || ntasks > 0;
+                        });
+                        if (tasks[level].empty()) {
+                            if (stop && ntasks == 0)
+                                return;
+                            continue;
+                        }
+                        t = tasks[level].front().release();
+                        tasks[level].pop();
+                        --ntasks;
+                        break;
+                    }
+                    schedule(t);
+                }
+            });
+        }
+    }
+    
+    ~mlfq() noexcept
+    {
+        stop = true;
+        for (std::thread &thrd : threads)
+            thrd.join();
+    }
+    
+    /*  
+     *  By default, a newly created task is insterted into the highest
+     *  priority queue (tasks[0]), but enqueue() is also called by a thread
+     *  reinserting a previously running task so the enqueue functions allow
+     *  for specifying a queue level; there is an exception for the argument
+     *  list enqueue function which can assume the task is newly created
+     */
+    void
+    enqueue(task *t, u32 level = 0) noexcept
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        tasks[level].emplace(t);
+        ++ntasks;
+    }
+
+    template<typename T>
+    void
+    enqueue(T &&t, u32 level = 0) noexcept
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        tasks[level].push(std::make_unique<task>(std::forward<T>(task)));
+        ++ntasks;
+    }
+    
+    template<typename T, typename ...Args>
+    void
+    enqueue(Args &&...args) noexcept
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        tasks[0].emplace(new T(std::foward<Args>(args)...));
+        ++ntasks;
+    }
+};
+} // namespace scheduler
